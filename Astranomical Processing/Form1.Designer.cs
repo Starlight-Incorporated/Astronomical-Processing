10b44a7f33654da96543e949a2271c2db57532b9
Andrew-29-code <M164821@tafe.wa.edu.au>
2023-05-06 13:14:41 +0800

    Astranomical Processing
    Form design 1

---
+++ Astranomical Processing/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Astranomical_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lstArrayDisplay = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 35);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(188, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(223, 35);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(223, 85);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // lstArrayDisplay
+            // 
+            this.lstArrayDisplay.FormattingEnabled = true;
+            this.lstArrayDisplay.Location = new System.Drawing.Point(12, 85);
+            this.lstArrayDisplay.Name = "lstArrayDisplay";
+            this.lstArrayDisplay.Size = new System.Drawing.Size(188, 160);
+            this.lstArrayDisplay.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 259);
+            this.Controls.Add(this.lstArrayDisplay);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Astranomical Processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.ListBox lstArrayDisplay;
+    }
+}
+
---
+++ Astranomical Processing/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Astranomical_Processing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] neutrinosArray = new int[24];
+    }
+}
