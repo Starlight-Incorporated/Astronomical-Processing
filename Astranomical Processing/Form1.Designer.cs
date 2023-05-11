21ccf3886fd1f1a45dd32217f4fc63d49ce7c83e
Andrew-29-code <M164821@tafe.wa.edu.au>
2023-05-11 10:10:11 +0800

    Update GUI Design
    Added a button used to enter typed data to the array

--- Astranomical Processing/Form1.Designer.cs
+++ Astranomical Processing/Form1.Designer.cs
@@ -32,32 +32,30 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnSort = new System.Windows.Forms.Button();
             this.lstArrayDisplay = new System.Windows.Forms.ListBox();
+            this.btnEnterData = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(16, 43);
-            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInput.Location = new System.Drawing.Point(12, 35);
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(249, 22);
+            this.txtInput.Size = new System.Drawing.Size(188, 20);
             this.txtInput.TabIndex = 0;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(297, 43);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Location = new System.Drawing.Point(228, 85);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(100, 28);
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // btnSort
             // 
-            this.btnSort.Location = new System.Drawing.Point(297, 105);
-            this.btnSort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSort.Location = new System.Drawing.Point(228, 135);
             this.btnSort.Name = "btnSort";
-            this.btnSort.Size = new System.Drawing.Size(100, 28);
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
             this.btnSort.TabIndex = 2;
             this.btnSort.Text = "Sort";
             this.btnSort.UseVisualStyleBackColor = true;
@@ -65,23 +63,30 @@
             // lstArrayDisplay
             // 
             this.lstArrayDisplay.FormattingEnabled = true;
-            this.lstArrayDisplay.ItemHeight = 16;
-            this.lstArrayDisplay.Location = new System.Drawing.Point(16, 105);
-            this.lstArrayDisplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstArrayDisplay.Location = new System.Drawing.Point(12, 85);
             this.lstArrayDisplay.Name = "lstArrayDisplay";
-            this.lstArrayDisplay.Size = new System.Drawing.Size(249, 196);
+            this.lstArrayDisplay.Size = new System.Drawing.Size(188, 160);
             this.lstArrayDisplay.TabIndex = 3;
+            // 
+            // btnEnterData
+            // 
+            this.btnEnterData.Location = new System.Drawing.Point(228, 35);
+            this.btnEnterData.Name = "btnEnterData";
+            this.btnEnterData.Size = new System.Drawing.Size(75, 23);
+            this.btnEnterData.TabIndex = 4;
+            this.btnEnterData.Text = "Enter";
+            this.btnEnterData.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 319);
+            this.ClientSize = new System.Drawing.Size(315, 259);
+            this.Controls.Add(this.btnEnterData);
             this.Controls.Add(this.lstArrayDisplay);
             this.Controls.Add(this.btnSort);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtInput);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Astronomical Processing";
             this.ResumeLayout(false);
@@ -95,6 +100,7 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnSort;
         private System.Windows.Forms.ListBox lstArrayDisplay;
+        private System.Windows.Forms.Button btnEnterData;
     }
 }
 
